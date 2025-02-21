e8101de0c1ae0af94df0a82ef41ac24a5f48c888
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-21 11:18:11 +0530

    section 5 slider done

--- assets/index.js/app.js
+++ assets/index.js/app.js
@@ -5,3 +5,22 @@ const navbarbtn = () => {
     document.getElementById("rotates-2").classList.toggle("rotate-43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+$('.my-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".previous",
+    nextArrow: ".next",
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        },
+    ]
+});
